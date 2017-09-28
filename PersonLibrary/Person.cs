9fd8721f6e635484f0eb6b64958d0403241fee33
MikeBlinov <t.blinova@inbox.ru>
2017-09-28 22:20:49 +0300

    It is the first commit

---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+
+namespace PersonLibrary
+{
+    public class Person : DependencyObject
+    {
+        public string LastName
+        {
+            get { return (string)GetValue(LastNameProperty); }
+            set { SetValue(LastNameProperty, value); }
+        }
+
+        public static DependencyProperty LastNameProperty =
+             DependencyProperty.Register("LastName", typeof(string), typeof(Person),
+             new PropertyMetadata("LastName Hello"));
+
+        public string FirstName
+        {
+            get { return (string)GetValue(FirstNameProperty); }
+            set { SetValue(FirstNameProperty, value); }
+        }
+
+        public static DependencyProperty FirstNameProperty =
+             DependencyProperty.Register("FirstName", typeof(string), typeof(Person),
+             new PropertyMetadata("LastName Hello"));
+
+        public int Age
+        {
+            get { return (int)GetValue(AgeProperty); }
+            set { SetValue(AgeProperty, value); }
+        }
+
+        public static DependencyProperty AgeProperty =
+             DependencyProperty.Register("Age", typeof(int), typeof(Person),
+             new PropertyMetadata(123));
+
+
+    }
+
+}
---
+++ PersonObject/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PersonLibrary;
+
+namespace PersonObject
+{
+
+
+    public partial class MainWindow : Window
+    {
+
+        private Person person = new Person() {Age = 11, LastName = "Last Name", FirstName = "First Name"};
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Grd.DataContext = person;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            person.Age = 22;
+            person.LastName = "Last Name xxxx";
+            person.FirstName = "First Name yyyyy";
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("person.Age = " + person.Age.ToString() + " " + "person.LastName = " + person.LastName + " " +
+                            "person.FirstName = " + person.FirstName);
+        }
+    }
+}
